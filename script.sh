f27c3299c833d52c6ba79aabab669214164068d9
ox-rock <54623882+ox-rock@users.noreply.github.com>
2020-07-13 10:02:46 -0700

    code refactoring

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+START_TIME=$SECONDS
+
 declare -A value=()
 declare -a IP=()
 
@@ -9,28 +11,33 @@ addIP() {
     value["$1_cloudflareDNS"]="$3"
     value["$1_adguardDNS"]="$4"
     value["$1_whois"]="$5"
-    value["$1_whoiscymru"]="$6"
-    value["$1_sni"]="$7"	
+    value["$1_whoiscymru"]="$6"	
 }
+
+tshark -r 2020_07_07_180408.pcap -E separator=, -T fields -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport -e ssl.handshake.extensions_server_name ssl.handshake.extensions_server_name > sni 
 
 for protocol in udp tcp; do
     tshark -r 2020_07_07_180408.pcap -qz conv,$protocol | head -n -1 | tail -n +6 | {
 
     echo "\"Address A\",\"Port A\",\"Address B\",\"Port B\",\"Packets B → A\",\"Bytes B → A\",\"Packets A → B\",\"Bytes A → B\",\"Total Packets\",\"Total Bytes\",\"OpenDNS\",\"Cloudflare DNS\",\"AdGuard  DNS\",\"WhoIs\",\"WhoIs (Cymru)\",\"SNI\"" >> ${protocol}_conv.txt
-    while IFS=' ' read -r Address_PortA f2 AddressB_PortB B_A_Frames B_A_Bytes A_B_Frames A_B_Bytes Total_Frames Total_bytes f10; do
-        printf "${Address_PortA//':'/,},${AddressB_PortB//':'/,},$B_A_Frames,$B_A_Bytes,$A_B_Frames,$A_B_Bytes,$Total_Frames,$Total_bytes"
+
+    while IFS=' ' read -r AddressA_PortA f2 AddressB_PortB B_A_Frames B_A_Bytes A_B_Frames A_B_Bytes Total_Frames Total_bytes f10; do
+        printf "${AddressA_PortA//':'/,},${AddressB_PortB//':'/,},$B_A_Frames,$B_A_Bytes,$A_B_Frames,$A_B_Bytes,$Total_Frames,$Total_bytes"
 
 	AddressB=$(echo $AddressB_PortB | cut -f1 -d:)
+	PortA=$(echo $AddressA_PortA | cut -f2 -d:)
 
 	bool="false"
 	for x in "${IP[@]}"; do
 	    if [ "$x" == "$AddressB" ]; then 
                 bool="true"
+                printf ",\"${value[${x}_openDNS]}\",\"${value[${x}_cloudflareDNS]}\",\"${value[${x}_adguardDNS]}\",\"${value[${x}_whois]}\",\"${value[${x}_whoiscymru]}\","
 	        break 
 	    fi
 	done
 
 	if [ "$bool" == "false" ]; then 
+	    #Reverse DNS resolution by OpenDNS, CloudFlare and AdGuard DNS
             openDNS=$(dig +short @208.67.222.222 -x ${AddressB})
 	    if [ -z "$openDNS" ]; then 
                 openDNS="null"
@@ -44,6 +51,7 @@ for protocol in udp tcp; do
                 adguardDNS="null"
 	    fi
 
+            #whois query
 	    whois=$(whois ${AddressB} | grep -i "org-name" | cut -f2 -d:)
 	    if [ -z "$whois" ]; then  
                 whois=$(whois ${AddressB} | grep -i "OrgName" | cut -f2 -d:)
@@ -55,30 +63,25 @@ for protocol in udp tcp; do
 	        fi
 	    fi
 	    
+            #whois query by Team Cymru
             whoiscymru=$(whois -h whois.cymru.com ${AddressB} | tail -n 1 | cut -f3 -d'|')
-	    
-	    sni=$(tshark -r 2020_07_07_180408.pcap -T fields -e ssl.handshake.extensions_server_name ip.dst==${AddressB} and ssl.handshake.extensions_server_name | tail -n 1)
-            if [ -z "$sni" ]; then
-                sni="null"
-	    fi
-	    
-            addIP $AddressB "$openDNS" "$cloudflareDNS" "$adguardDNS" "$whois" "$whoiscymru" $sni 
+	       
+            addIP $AddressB "$openDNS" "$cloudflareDNS" "$adguardDNS" "$whois" "$whoiscymru"
+	    printf ",\"${openDNS}\",\"${cloudflareDNS}\",\"${adguardDNS}\",\"${whois}\",\"${whoiscymru}\"," 
 	fi
 
-	for x in "${IP[@]}"; do
-	    if [ "$x" == "$AddressB" ]; then
-                printf ",\"${value[${x}_openDNS]}\",\"${value[${x}_cloudflareDNS]}\",\"${value[${x}_adguardDNS]}\",\"${value[${x}_whois]}\",\"${value[${x}_whoiscymru]}\",\"${value[${x}_sni]}\"\n"
-		break
-	    fi
-	done
+    	#SNI extraction from pcap
+	sni=$(cat sni | grep ${AddressB} | grep ${PortA} | cut -f5 -d',')
+        if [ -z "$sni" ]; then
+            sni="null"
+	fi
+	printf "$sni\n"
 
     done >> ${protocol}_conv.txt
 }
 
 done
 
-
-
-
-
-
+rm sni
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo -e "Tempo di elaborazione: $(($ELAPSED_TIME%60))s"
