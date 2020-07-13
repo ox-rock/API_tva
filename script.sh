9a948fc53f1bbfb0b41dba23d51e18d44bb19dc5
ox-rock <54623882+ox-rock@users.noreply.github.com>
2020-07-13 16:54:42 -0700

    code refactoring

--- script.sh
+++ script.sh
@@ -14,18 +14,30 @@ addIP() {
     value["$1_whoiscymru"]="$6"	
 }
 
-tshark -r 2020_07_07_180408.pcap -E separator=, -T fields -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport -e ssl.handshake.extensions_server_name ssl.handshake.extensions_server_name > sni 
+tshark -r 2020_07_07_180408.pcap -E separator=, -T fields -e tcp.stream -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport -e frame.time_relative -e ssl.handshake.extensions_server_name > sni 
 
 for protocol in udp tcp; do
+    cnt=$(tshark -r 2020_07_07_180408.pcap -qz conv,$protocol | head -n -1 | tail -n +6 | wc -l)
     tshark -r 2020_07_07_180408.pcap -qz conv,$protocol | head -n -1 | tail -n +6 | {
 
-    echo "\"Address A\",\"Port A\",\"Address B\",\"Port B\",\"Packets B → A\",\"Bytes B → A\",\"Packets A → B\",\"Bytes A → B\",\"Total Packets\",\"Total Bytes\",\"OpenDNS\",\"Cloudflare DNS\",\"AdGuard  DNS\",\"WhoIs\",\"WhoIs (Cymru)\",\"SNI\"" >> ${protocol}_conv.txt
-
-    while IFS=' ' read -r AddressA_PortA f2 AddressB_PortB B_A_Frames B_A_Bytes A_B_Frames A_B_Bytes Total_Frames Total_bytes f10; do
-        printf "${AddressA_PortA//':'/,},${AddressB_PortB//':'/,},$B_A_Frames,$B_A_Bytes,$A_B_Frames,$A_B_Bytes,$Total_Frames,$Total_bytes"
-
+    echo "\"ID\",\"Address A\",\"Port A\",\"Address B\",\"Port B\",\"Packets B → A\",\"Bytes B → A\",\"Packets A → B\",\"Bytes A → B\",\"Total Packets\",\"Total Bytes\",\"Start time\",\"Duration\",\"nslookup\",\"OpenDNS\",\"Cloudflare DNS\",\"AdGuard  DNS\",\"WhoIs\",\"WhoIs (Cymru)\",\"SNI\"" >> ${protocol}_conv.txt
+    i=1
+    while IFS=' ' read -r AddressA_PortA f2 AddressB_PortB B_A_Frames B_A_Bytes A_B_Frames A_B_Bytes Total_Frames Total_bytes start duration; do
+	
 	AddressB=$(echo $AddressB_PortB | cut -f1 -d:)
 	PortA=$(echo $AddressA_PortA | cut -f2 -d:)
+
+	id=$(cat sni | grep ${start} | cut -f1 -d',')
+
+	printf "\rProcessing [$i/$cnt] $protocol conversation..." 1>&2
+	i=$((i+1))
+        printf "$id,${AddressA_PortA//':'/,},${AddressB_PortB//':'/,},$B_A_Frames,$B_A_Bytes,$A_B_Frames,$A_B_Bytes,$Total_Frames,$Total_bytes, $start, $duration"
+
+	nslookup=$(cat nslookup.log | grep ${AddressB} | cut -f2 -d',')
+        if [ -z "$nslookup" ]; then 
+            nslookup="null"
+	fi
+        printf ",$nslookup"
 
 	bool="false"
 	for x in "${IP[@]}"; do
@@ -71,17 +83,18 @@ for protocol in udp tcp; do
 	fi
 
     	#SNI extraction from pcap
-	sni=$(cat sni | grep ${AddressB} | grep ${PortA} | cut -f5 -d',')
+	sni=$(cat sni | grep ${id}, | grep ,${AddressB}, | grep ,${PortA}, | cut -f7 -d',' | uniq | head -n 2 | tail -n 1 )
         if [ -z "$sni" ]; then
             sni="null"
 	fi
 	printf "$sni\n"
 
     done >> ${protocol}_conv.txt
+	printf "\n"
 }
 
 done
 
 rm sni
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
-echo -e "Tempo di elaborazione: $(($ELAPSED_TIME%60))s"
+printf "\nElapsed time: $(($ELAPSED_TIME%60))s\n"
