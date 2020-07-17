69fa2e4fcadf2293e90ac82f8969b1624b5e5f8f
ox-rock <54623882+ox-rock@users.noreply.github.com>
2020-07-17 13:51:13 -0700

    add android DNS

--- script.sh
+++ script.sh
@@ -15,32 +15,40 @@ addIP() {
 }
 
 pcap_file=$(find . -type f -name '*.pcap' -or -name '*.pcapng')
-
 printf "Reading $pcap_file\n"
 
-for protocol in udp tcp; do
-    tshark -r $pcap_file -Y "ip and $protocol" -E separator=, -T fields -e $protocol.stream -e ip.src -e $protocol.srcport -e ip.dst -e $protocol.dstport -e frame.time_relative -e ssl.handshake.extensions_server_name > ${protocol}_sni 
+   tshark -r $pcap_file -E separator=, -T fields -e tcp.stream -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport -e frame.time_relative -e ssl.handshake.extensions_server_name -e http.host -Y "ip" > temp_tcp 
 
+   tshark -r $pcap_file -E separator=, -T fields -e udp.stream -e ip.src -e udp.srcport -e ip.dst -e udp.dstport -e frame.time_relative -e http.host -Y "ip" > temp_udp 
+
+   tshark -r $pcap_file -E separator=/s -T fields -e dns.a -e dns.qry.name -Y "(dns.flags.response == 1 )" > temp_dns
+
+conv2csv() {
     cnt=$(tshark -r $pcap_file -qz conv,$protocol,ip | head -n -1 | tail -n +6 | wc -l)
 
     tshark -r $pcap_file -qz conv,$protocol,ip | head -n -1 | tail -n +6 | {
 
-    echo "\"ID\",\"Address A\",\"Port A\",\"Address B\",\"Port B\",\"Packets B → A\",\"Bytes B → A\",\"Packets A → B\",\"Bytes A → B\",\"Total Packets\",\"Total Bytes\",\"Start time\",\"Duration\",\"nslookup\",\"OpenDNS\",\"Cloudflare DNS\",\"AdGuard  DNS\",\"WhoIs\",\"WhoIs (Cymru)\",\"SNI\"" >> ${protocol}_conv.txt
+    echo "\"ID\",\"Address A\",\"Port A\",\"Address B\",\"Port B\",\"Packets B → A\",\"Bytes B → A\",\"Packets A → B\",\"Bytes A → B\",\"Total Packets\",\"Total Bytes\",\"Start time\",\"Duration\",\"Android DNS\",\"nslookup\",\"OpenDNS\",\"Cloudflare DNS\",\"AdGuard DNS\",\"WhoIs\",\"WhoIs (Cymru)\",\"SNI\",\"Host HTTP\"" >> ${protocol}_conv.txt
     i=1
     while IFS=' ' read -r AddressA_PortA f2 AddressB_PortB B_A_Frames B_A_Bytes A_B_Frames A_B_Bytes Total_Frames Total_bytes start duration; do
-	
+
 	AddressB=$(echo $AddressB_PortB | cut -f1 -d:)
 	PortA=$(echo $AddressA_PortA | cut -f2 -d:)
-
 	id=$(cat ${protocol}_sni | grep ${AddressB} | grep ${start} | uniq | cut -f1 -d',')
-
+	
 	printf "\rProcessing [$i/$cnt] $protocol conversation" 1>&2
-	i=$((i+1))
+
         printf "$id,${AddressA_PortA//':'/,},${AddressB_PortB//':'/,},$B_A_Frames,$B_A_Bytes,$A_B_Frames,$A_B_Bytes,$Total_Frames,$Total_bytes,$start,$duration"
 
 	if [[ "$AddressB" == 192.168.* || "$AddressB" == "239.255.255.250" || "$AddressB" == 224.0.0.* || "$AddressB" == "255.255.255.255" ]]; then 
-	    printf ",-,-,-,-,-,-,-\n"
+	    printf ",-,-,-,-,-,-,-,-,-\n"
 	else
+	    androidDNS=$(cat temp_dns | grep ${AddressB} | cut -f2 -d' ' | uniq | tr '\n' ' ')
+            if [ -z "$androidDNS" ]; then 
+                androidDNS="null"
+	    fi
+	    printf ",$androidDNS"
+
 	    nslookup=$(cat nslookup.log | grep ${AddressB} | cut -f2 -d',')
             if [ -z "$nslookup" ]; then 
                 nslookup="null"
@@ -90,24 +98,37 @@ for protocol in udp tcp; do
 		printf ",\"$openDNS\",\"$cloudflareDNS\",\"$adguardDNS\",\"$whois\",\"$whoiscymru\"," 
 	    fi
 
-	    #SNI extraction
+	    #SNI & Host HTTP extraction
 	    if [ "$protocol" == "tcp" ]; then 	
-		sni=$(cat ${protocol}_sni | grep ${id} | grep ${AddressB} | grep ${PortA} | cut -f7 -d',' | uniq | head -n 2 | tail -n 1 )
+		sni=$(cat temp_tcp | grep ${id} | grep ${AddressB} | grep ${PortA} | cut -f7 -d',' | uniq | head -n 2 | tail -n 1 )
 		if [ -z "$sni" ]; then
 		    sni="null"
 		fi
-		printf "$sni\n"
+	        host=$(cat temp_tcp | grep ${id} | grep ${AddressB} | grep ${PortA} | cut -f8 -d',' | uniq | head -n 2 | tail -n 1 )
+	        if [ -z "$host" ]; then
+		    host="null"
+	        fi
+	        printf "$sni,$host\n"
 	    else 
-		sni="null"
-		printf "$sni\n"
-	    fi
+	        host=$(cat temp_udp | grep ${id} | grep ${AddressB} | grep ${PortA} | cut -f7 -d',' | uniq | head -n 2 | tail -n 1 )
+	        if [ -z "$host" ]; then
+		    host="null"
+	        fi
+		printf -- "-,$host\n"
+	    fi	
         fi
-	
+	i=$((i+1))
         done >> ${protocol}_conv.txt
-	printf "\n"
+	rm ${protocol}_sni
+    }
 }
-rm ${protocol}_sni
+
+for protocol in udp tcp; do
+    conv2csv &
 done
 
+wait
+
+rm dns
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 printf "\nElapsed time: $(($ELAPSED_TIME))s\n"
